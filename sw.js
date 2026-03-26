ac6b1a2a88cf3f881d15d2d0f18804334655aa2b
Анастасия Шевко <nastingtoon@MacBook-Air-Nastingtoon.local>
2026-03-27 01:57:45 +0300

    fix5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='story-text-editor-v1';
+const CACHE='story-text-editor-v5';
 const ASSETS=['./','./index.html','./manifest.json','./Shevko-Regular.ttf','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate', e=>{e.waitUntil(self.clients.claim());});
