0b9c8f1270d56bb072ba9edd7c3b25d9bcb24122
Анастасия Шевко <nastingtoon@MacBook-Air-Nastingtoon.local>
2026-03-27 12:29:54 +0300

    crop fix

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-const CACHE='story-text-editor-v5';
-const ASSETS=['./','./index.html','./manifest.json','./Shevko-Regular.ttf','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
-self.addEventListener('activate', e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request).catch(()=>caches.match('./index.html'))));
+const CACHE = 'story-text-editor-v7';
+const ASSETS = ['./', './index.html', './manifest.json', './Shevko-Regular.ttf', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match('./index.html'))));
 });
